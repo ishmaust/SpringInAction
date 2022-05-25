d9736f8d7c3ec73a1eb96b19d2d4ca4e5006c424
Ilya Shmaust <ilyashmaust1999@gmail.com>
2022-05-25 22:36:05 +0300

    Initial commit

---
+++ src/test/java/user/ishmaust/SpringInAction/SpringInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package user.ishmaust.SpringInAction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
